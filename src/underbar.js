d70223d34a40876658ec8c09fac32f8843ed7e9d
Festus Kiprop <fyangani@gmail.com>
2015-10-12 21:57:10 -0500

    Fourth commit, reduce still having issues

--- src/underbar.js
+++ src/underbar.js
@@ -159,6 +159,11 @@
   //   }); // should be 5, regardless of the iterator function passed in
   //          No accumulator is given so the first element is used.
   _.reduce = function(collection, iterator, accumulator) {
+    accumulator = accumulator || 0;
+    _.each(collection, function(value) {
+      accumulator = iterator(accumulator, value);
+    })
+    return accumulator;
   };
 
   // Determine if the array or object contains a given value (using `===`).
